e66159fca2357ec67308435e049380f74af33508
Kent Jones <jones.kent@pm.me>
2021-09-20 23:42:04 -0700

    add architecture img

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get('/', (request, response) => {
         <html lang="en">
         <head>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport"content="width=device-width, initial-scale=1" />
           <title>PiO1 Hosted Website</title>
           <style>
             img {
-              width: 900px;
+              width: 800px;
               height: auto;
             }
           </style>
