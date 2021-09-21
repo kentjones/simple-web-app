6b08845d4a206c328f3c03dcc5a0686f7fcdab4f
Kent Jones <jones.kent@pm.me>
2021-09-21 12:06:56 -0700

    removed root uri from  express.static('public')

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
   
